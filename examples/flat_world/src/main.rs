7be8602bec825173bde31b1c9013f503029e145c
VilleOlof <villeolof@gmail.com>
2025-07-30 02:46:38 +0200

    simplified flat_world example

--- examples/flat_world/src/main.rs
+++ examples/flat_world/src/main.rs
@@ -1,4 +1,4 @@
-use rust_edit::{Block, Name, Region, Result};
+use rust_edit::{Region, Result};
 use std::fs::File;
 
 const BLOCKS_PER_REGION: u32 = 512;
@@ -6,16 +6,12 @@ const BLOCKS_PER_REGION: u32 = 512;
 pub fn main() -> Result<()> {
     let mut region = Region::full_empty((0, 0));
 
-    let bedrock = Block::try_new(Name::new_namespace("minecraft:bedrock"))?;
-    let dirt = Block::try_new(Name::new_namespace("minecraft:dirt"))?;
-    let grass_block = Block::try_new(Name::new_namespace("minecraft:grass_block"))?;
-
     for x in 0..BLOCKS_PER_REGION {
         for z in 0..BLOCKS_PER_REGION {
-            region.set_block(x, 0, z, bedrock.clone());
-            region.set_block(x, 1, z, dirt.clone());
-            region.set_block(x, 2, z, dirt.clone());
-            region.set_block(x, 3, z, grass_block.clone());
+            region.set_block(x, 0, z, "minecraft:bedrock");
+            region.set_block(x, 1, z, "minecraft:dirt");
+            region.set_block(x, 2, z, "minecraft:dirt");
+            region.set_block(x, 3, z, "minecraft:grass_block");
         }
     }
 
